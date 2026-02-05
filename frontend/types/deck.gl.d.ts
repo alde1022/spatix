3a7586effbf42fb62986adacee7879d4fa7f8dc5
Jarvis <jarvis@spatix.io>
2026-02-05 18:24:18 +0000

    fix: add TypeScript declarations for deck.gl modules

---
+++ frontend/types/deck.gl.d.ts
@@ -0,0 +1,47 @@
+declare module '@deck.gl/mapbox' {
+  export class MapboxOverlay {
+    constructor(props: any);
+    setProps(props: any): void;
+  }
+}
+
+declare module '@deck.gl/core' {
+  export class Deck {
+    constructor(props: any);
+    setProps(props: any): void;
+    finalize(): void;
+  }
+}
+
+declare module '@deck.gl/layers' {
+  export class ScatterplotLayer {
+    constructor(props: any);
+  }
+  export class ArcLayer {
+    constructor(props: any);
+  }
+  export class GeoJsonLayer {
+    constructor(props: any);
+  }
+  export class LineLayer {
+    constructor(props: any);
+  }
+  export class PolygonLayer {
+    constructor(props: any);
+  }
+}
+
+declare module '@deck.gl/aggregation-layers' {
+  export class HexagonLayer {
+    constructor(props: any);
+  }
+  export class HeatmapLayer {
+    constructor(props: any);
+  }
+}
+
+declare module '@deck.gl/geo-layers' {
+  export class H3HexagonLayer {
+    constructor(props: any);
+  }
+}
